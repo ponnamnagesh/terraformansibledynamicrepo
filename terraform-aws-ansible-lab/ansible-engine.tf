9899328c347c9febd22eae03fa126752a78b9aa4
ginigangadharan <net.gini@gmail.com>
2022-01-26 18:58:42 +0800

    update

--- terraform-aws-ansible-lab/ansible-engine.tf
+++ terraform-aws-ansible-lab/ansible-engine.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "ansible-engine" {
   key_name      = aws_key_pair.ec2loginkey.key_name
   ## If you are creating Instances in a VPC, use vpc_security_group_ids instead.
   security_groups = ["ansible-lab-sg"]
-  user_data = file("user-data-ansible-engine.sh")
+  user_data       = file("user-data-ansible-engine.sh")
 
   # Create inventory and ansible.cfg on ansible-engine
   provisioner "remote-exec" {
@@ -23,7 +23,7 @@ resource "aws_instance" "ansible-engine" {
       "echo '#ansible_python_interpreter=/usr/bin/python3' >> /home/ec2-user/inventory",
       "echo 'ansible_ssh_private_key_file=/home/devops/.ssh/id_rsa' >> /home/ec2-user/inventory",
       "echo \"ansible_ssh_extra_args=' -o StrictHostKeyChecking=no -o PreferredAuthentications=password '\" >> /home/ec2-user/inventory",
-      "echo '[defaults]' >> /home/ec2-user/ansible.cfg", 
+      "echo '[defaults]' >> /home/ec2-user/ansible.cfg",
       "echo 'inventory = ./inventory' >> /home/ec2-user/ansible.cfg",
       "echo 'host_key_checking = False' >> /home/ec2-user/ansible.cfg",
       "echo 'remote_user = devops' >> /home/ec2-user/ansible.cfg",
@@ -33,6 +33,7 @@ resource "aws_instance" "ansible-engine" {
       user        = "ec2-user"
       private_key = file(pathexpand(var.ssh_key_pair))
       host        = self.public_ip
+      agent       = false
     }
   }
 
@@ -41,10 +42,10 @@ resource "aws_instance" "ansible-engine" {
     source      = "engine-config.yaml"
     destination = "/home/ec2-user/engine-config.yaml"
     connection {
-      type = "ssh"
-      user = "ec2-user"
-      private_key = file(pathexpand(var.ssh_key_pair))      
-      host = self.public_ip
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file(pathexpand(var.ssh_key_pair))
+      host        = self.public_ip
     }
   }
 
@@ -54,10 +55,10 @@ resource "aws_instance" "ansible-engine" {
       "sleep 120; ansible-playbook engine-config.yaml"
     ]
     connection {
-      type = "ssh"
-      user = "ec2-user"
-      private_key = file(pathexpand(var.ssh_key_pair))      
-      host = self.public_ip
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file(pathexpand(var.ssh_key_pair))
+      host        = self.public_ip
     }
   }
 
--- terraform-aws-ansible-lab/variables.tf
+++ terraform-aws-ansible-lab/variables.tf
@@ -6,10 +6,12 @@ variable "aws_ami_id" {
 
 variable "ssh_key_pair" {
   default = "~/.ssh/id_rsa"
+  #default = "~/.ssh/id_rsa_ansilble_lab"
 }
 
 variable "ssh_key_pair_pub" {
   default = "~/.ssh/id_rsa.pub"
+  #default = "~/.ssh/id_rsa_ansilble_lab.pub"
 }
 
 variable "ansible_node_count" {
