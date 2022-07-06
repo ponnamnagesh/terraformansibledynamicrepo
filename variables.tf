5ce1d6090b33f435856cd63519fc71c6ce43ea01
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-07-06 10:25:30 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,8 @@
 variable "aws_ami_id" {
   ## Amazon Linux 2 AMI (HVM)
-  default = "ami-02f26adf094f51167"
+  ##default = "ami-02f26adf094f51167"
+  default = "ami-05803413c51f242b7"
+
   ## "ami-0cd31be676780afa7"
 }
 
