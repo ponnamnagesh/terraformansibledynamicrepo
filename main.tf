77f1f42a96c0759a24d8545bf456d2aab6ab759e
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-07-06 10:04:14 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,11 +6,11 @@ provider "aws" {
   #version                 = ">=2.0"
 }
 
-resource "aws_key_pair" "ec2loginkey" {
-  key_name = "login-key"
+##resource "aws_key_pair" "ec2loginkey" {
+ ## key_name = "login-key"
   ## change here if you are using different key pair
-  public_key = file(pathexpand(var.ssh_key_pair_pub))
-}
+ ## public_key = file(pathexpand(var.ssh_key_pair_pub))
+##}
 
 output "ansible-engine" {
   value = aws_instance.ansible-engine.public_ip
