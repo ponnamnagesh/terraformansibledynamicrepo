4a51b47ace6a20f523956222f5458544a6917564
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-07-06 10:08:37 -0500

    Update ansibleengine.tf

--- ansibleengine.tf
+++ ansibleengine.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "ansible-engine" {
   ami           = var.aws_ami_id #"ami-0cd31be676780afa7"
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.ec2loginkey.key_name
+  ##key_name      = aws_key_pair.ec2loginkey.key_name
   ## If you are creating Instances in a VPC, use vpc_security_group_ids instead.
   security_groups = ["ansible-lab-sg"]
   user_data = file("user-data-ansible-engine.sh")
